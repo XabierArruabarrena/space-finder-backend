b193e7c71456ab521eabf3746dbb76cc23d0f2da
XabierArruabarrena <arruabarrenaxabi@gmail.com>
2021-12-08 16:45:08 +0100

    Testing Lambdas

--- lib/SpaceStacks.ts
+++ lib/SpaceStacks.ts
@@ -1,9 +1,13 @@
 import * as cdk from '@aws-cdk/core';
-import * as lambda from '@aws-cdk/aws-lambda'
-import { Code, Runtime } from '@aws-cdk/aws-lambda'
+/* import * as lambda from '@aws-cdk/aws-lambda'
+import { Code, Runtime } from '@aws-cdk/aws-lambda' */
 import { join } from 'path'
 import * as apigw from '@aws-cdk/aws-apigateway'
 import { GenericTable } from './GenericTable';
+import { NodejsFunction } from '@aws-cdk/aws-lambda-nodejs'
+import { handler } from '../services/node-lambda/hello';
+import { PolicyStatement } from '@aws-cdk/aws-iam';
+
 // import * as sqs from '@aws-cdk/aws-sqs';
 
 export class SpaceStack extends cdk.Stack {
@@ -18,15 +22,29 @@ private spacesTable = new GenericTable(
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const helloLambda = new lambda.Function(this, 'helloLambda', {
+    /* const helloLambda = new lambda.Function(this, 'helloLambda', {
       runtime: Runtime.NODEJS_14_X,
       code: Code.fromAsset(join(__dirname, '..','services', 'hello')),
       handler: 'hello.main'
+    }) */
+
+
+
+    const helloLambdaNodeJs = new NodejsFunction(this, 'helloLambdaNodeJs', {
+      entry: (join(__dirname, '..', 'services', 'node-lambda', 'hello.ts')),
+      handler: 'handler',
+
     })
+
+    const s3ListPolicy = new PolicyStatement();
+    s3ListPolicy.addActions('s3:ListAllMyBuckets');
+    s3ListPolicy.addNotResources('*');
+    helloLambdaNodeJs.addToRolePolicy(s3ListPolicy);
+
 
 
     //Hello Api Lambda integration: 
-    const helloLambdaIntegration = new apigw.LambdaIntegration(helloLambda)
+    const helloLambdaIntegration = new apigw.LambdaIntegration(helloLambdaNodeJs)
     const helloLambdaResource = this.api.root.addResource('hello');
     helloLambdaResource.addMethod('GET', helloLambdaIntegration)
 
